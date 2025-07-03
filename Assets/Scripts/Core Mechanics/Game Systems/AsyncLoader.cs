ec77ac356d59c47b691fd1a8a7606f51b774a887
tim <155276022+trcrzs@users.noreply.github.com>
2025-07-03 11:46:12 -0700

    Created Main Menu and Loading Screen
    Main menu and loading screen have temporary assets to set up functionality.

---
+++ Assets/Scripts/Core Mechanics/Game Systems/AsyncLoader.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class AsyncLoader : MonoBehaviour
+{
+    [Header("Menu Screens")]
+    [SerializeField] private GameObject loadingScreen;
+    [SerializeField] private GameObject mainMenu;
+
+    [Header("Slider")]
+    [SerializeField] private Slider loadingSlider;
+
+    public void LoadLevelBtn(string levelToLoad)
+    {
+        mainMenu.SetActive(false);
+        loadingScreen.SetActive(true);
+
+        StartCoroutine(LoadLevelAsync(levelToLoad));
+    }
+
+    IEnumerator LoadLevelAsync(string levelToLoad)
+    {
+        AsyncOperation loadOperation = SceneManager.LoadSceneAsync(levelToLoad);
+        loadOperation.allowSceneActivation = false;
+
+        while (loadOperation.progress < 0.9f)
+        {
+            float progressValue = Mathf.Clamp01(loadOperation.progress / 0.9f);
+            loadingSlider.value = progressValue;
+            yield return null;
+        }
+
+        // Scene is loaded, hold for a few seconds before activation
+        loadingSlider.value = 1f;
+        yield return new WaitForSeconds(2f);  // <--- Delay duration here
+
+        loadOperation.allowSceneActivation = true;
+    }
+
+}
