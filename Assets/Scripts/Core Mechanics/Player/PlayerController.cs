fccaba3b8126044878ac8a730c3b1e27383b2394
tim <155276022+trcrzs@users.noreply.github.com>
2025-07-29 19:45:52 -0700

    created grayboxes, updated player controller with jump input, created camera follow script

--- Assets/Scripts/Core Mechanics/Player/PlayerController.cs
+++ Assets/Scripts/Core Mechanics/Player/PlayerController.cs
@@ -1,27 +1,63 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
-using UnityEngine.SceneManagement;
 using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
+
+    //public variables
     public Rigidbody rb;
     public float moveSpeed = 5f;
-    public InputActionReference move;
+    public float jumpForce = 5f;
+    public Transform groundCheck;
+    public float groundDistance = 0.2f;
+    public LayerMask groundMask;
 
+    // input action items
+    public InputActionReference move;
+    public InputActionReference jump;
+    public InputActionReference fire;
+
+    // private variables
     private Vector3 moveDirection;
+    private bool isGrounded;
+    private bool jumpPressed;
 
     void Start()
     {
-        move.action.Enable(); // Make sure input is enabled
+
+        //input enablers
+        move.action.Enable();
+        jump.action.Enable();
+        fire.action.Enable();
+
+        //jump input callback
+        jump.action.performed += ctx => OnJump();
+        jump.action.canceled += ctx => jumpPressed = false;
+    }
+
+    private void OnDestroy()
+    {
+        jump.action.performed -= ctx => OnJump();
+        jump.action.canceled -= ctx => jumpPressed = false;
+    }
+
+    void OnJump()
+    {
+        //checks if player is on ground
+        if (isGrounded)
+        {
+            jumpPressed = true;
+        }
     }
 
     void Update()
     {
-        Vector2 input = move.action.ReadValue<Vector2>();
-        moveDirection = new Vector3(input.x, 0f, input.y);
+        Vector2 moveInput = move.action.ReadValue<Vector2>();
+        moveDirection = new Vector3(moveInput.x, 0f, moveInput.y);
+
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
     }
 
     void FixedUpdate()
@@ -31,5 +67,11 @@ public class PlayerController : MonoBehaviour
             rb.velocity.y,
             moveDirection.z * moveSpeed
         );
+
+        if (jumpPressed)
+        {
+            rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+            jumpPressed = false;
+        }
     }
 }
