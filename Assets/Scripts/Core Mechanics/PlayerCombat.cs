2b9423872520d6573f6c9a3f883d4dbeb49d728a
tim <155276022+trcrzs@users.noreply.github.com>
2025-09-17 12:09:16 -0700

    updating player combat

---
+++ Assets/Scripts/Core Mechanics/PlayerCombat.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCombat : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    /*function to melee
+    player will press J to melee
+    */
+
+    public void OnMelee() 
+    {
+
+    }
+
+    /*function to dodge
+    player will press the following:
+    A + left arrow to dodge left
+    S + down arrow to dodge down
+    D + right arrow to dodge right
+    */
+
+    public void OnDodge() 
+    {
+
+    }
+
+    /*function to reference special abilities class
+    player will use j to activate special abilities controller
+    SpecialAbilities.cs will handle this
+    */
+
+    public void OnSpecialAbilityController()
+    {
+        //reference special abilities class
+    }
+
+
+}
