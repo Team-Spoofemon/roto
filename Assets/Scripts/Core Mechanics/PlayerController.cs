90c32b3d5c45ec419f8931e5143fca6caab9a0a6
Nathan <nathan.a.valenzuela@gmail.com>
2025-09-22 23:02:55 -0700

    Update PlayerController.cs
    made my own movement system, still work in progress. can sprint, jump, and walk

--- Assets/Scripts/Core Mechanics/PlayerController.cs
+++ Assets/Scripts/Core Mechanics/PlayerController.cs
@@ -5,74 +5,73 @@ using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
+    private Rigidbody rb;
+    private PlayerInput PlayerInput;
+    private InputAction jumpAction;
+    private InputAction moveAction;
+    private InputAction sprintAction;
 
-    //public variables
-    public Rigidbody rb;
-    public float moveSpeed = 6f;
-    public float jumpForce = 5f;
-    public Transform groundCheck;
-    public float groundDistance = 0.2f;
-    public LayerMask groundMask;
+    private Vector2 moveAmt;
 
-    // input action items
-    public InputActionReference move;
-    public InputActionReference jump;
-    public InputActionReference fire;
+    private float walkSpeed = 5f;
+    private float jumpSpeed = 5f;
+    private float sprintSpeed = 10f;
 
-    // private variables
-    private Vector3 moveDirection;
-    private bool isGrounded;
-    private bool jumpPressed;
 
-    void Start()
+    public void Awake()
     {
+        // Gets Components
+        PlayerInput = GetComponent<PlayerInput>();
+        rb = GetComponent<Rigidbody>();
 
-        //input enablers
-        move.action.Enable();
-        jump.action.Enable();
-        fire.action.Enable();
+        // Looks inside input action map
+        moveAction = PlayerInput.actions["Move"];
+        jumpAction = PlayerInput.actions["Jump"];
+        sprintAction = PlayerInput.actions["Sprint"];
 
-
-        //jump input callback
-        jump.action.performed += ctx => OnJump();
-        jump.action.canceled += ctx => jumpPressed = false;
     }
 
-    private void OnDestroy()
+    public void Start()
     {
-        jump.action.performed -= ctx => OnJump();
-        jump.action.canceled -= ctx => jumpPressed = false;
+        Debug.Log("Game Started!");
     }
 
-    void OnJump()
+    // Updates every frame
+    public void Update()
     {
-        //checks if player is on ground
-        if (isGrounded)
+        // Reads the "move" input
+        moveAmt = moveAction.ReadValue<Vector2>();
+
+        // Jump if jump was pressed in this frame
+        if (jumpAction.WasPressedThisFrame())
         {
-            jumpPressed = true;
+            Jump();
         }
     }
 
-    void Update()
+    public void FixedUpdate()
     {
-        Vector2 moveInput = move.action.ReadValue<Vector2>();
-        moveDirection = new Vector3(moveInput.x, 0f, moveInput.y);
-
-        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+        // Calls Move every physics step (50x/sec)
+        Move();
+    }
+    public void Jump()
+    {
+        // Instant force upward (0, 1, 0)
+        rb.AddForce(Vector3.up * jumpSpeed, ForceMode.Impulse);
     }
 
-    void FixedUpdate()
+    public void Move()
     {
-        rb.velocity = new Vector3(
-            moveDirection.x * moveSpeed,
-            rb.velocity.y,
-            moveDirection.z * moveSpeed
-        );
+        // Check if sprint is held
+        bool isSprinting = sprintAction.IsPressed();
 
-        if (jumpPressed)
-        {
-            rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
-            jumpPressed = false;
-        }
+        // if/else for sprinting or walking
+        float moveSpeed = isSprinting ? sprintSpeed : walkSpeed;
+        
+        //Creates movement direction vector and multiplies by speed and delta time
+        Vector3 move = (transform.forward * moveAmt.y + transform.right * moveAmt.x) * moveSpeed * Time.deltaTime;
+        rb.MovePosition(rb.position + move);
     }
+
+
 }
