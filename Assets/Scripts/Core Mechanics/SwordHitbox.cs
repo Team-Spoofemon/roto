5f8e9097576325590161967b25f64d1a98ecdbfd
tim <155276022+trcrzs@users.noreply.github.com>
2025-10-07 15:33:47 -0700

    update player combat cs and create sword hitbox cs

---
+++ Assets/Scripts/Core Mechanics/SwordHitbox.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwordHitbox : MonoBehaviour
+{
+    [SerializeField] private Collider swordHitbox;
+    private PlayerCombat playerCombat;
+    //private EnemyHealth enemyHealth;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.layer == LayerMask.NameToLayer("Enemy"))
+        {
+            //other.GetComponent<EnemyHealth>().TakeDamage(playerCombat.AttackDamage);
+        }
+    }
+
+
+    
+}
